6860c2c02b1c84231ffdf88e956cacb95464fc9b
mwongeraE <evansmwongera@gmail.com>
2016-03-20 21:12:03 +0300

    adapter

--- app/src/main/java/com/example/mwongera/games/LazyAdapter.java
+++ app/src/main/java/com/example/mwongera/games/LazyAdapter.java
@@ -1,7 +1,48 @@
 package com.example.mwongera.games;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Created by mwongera on 3/20/16.
- */
-public class LazyAdapter {
-}
+import java.util.List;
+
+public class LazyAdapter extends ArrayAdapter<rowitem> {
+
+    Context context;
+
+    public LazyAdapter(Context context, int resourceId, List<rowitem> items){
+        super(context, resourceId, items);
+        this.context = context;
+    }
+
+    public class ViewHolder{
+        ImageView image;
+        TextView title;
+        TextView description;
+    }
+
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder holder;
+        RowItem rowItem = getItem(position);
+
+        LayoutInflater mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null){
+            convertView = mInflater.inflate(R.layout.list_row, null);
+            holder = new ViewHolder();
+            holder.image = (ImageView)convertView.findViewById(R.id.list_image);
+            holder.title = (TextView)convertView.findViewById(R.id.title);
+            holder.description = (TextView)convertView.findViewById(R.id.description);
+            convertView.setTag(holder);
+        } else
+            holder = (ViewHolder)convertView.getTag();
+
+        holder.image.setImageResource(rowItem.getImageId());
+        holder.title.setText(rowItem.getTitle());
+        holder.description.setText(rowItem.getDesc());
+
+        return convertView;
+    }
