f90ca1282d5942a88a9fc4d44f99407292e24488
mwongeraE <evansmwongera@gmail.com>
2016-03-20 18:09:15 +0300

    new app intro app intro

--- app/src/main/java/com/example/mwongera/games/MyIntro.java
+++ app/src/main/java/com/example/mwongera/games/MyIntro.java
@@ -1,7 +1,63 @@
 package com.example.mwongera.games;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
 /**
  * Created by mwongera on 3/20/16.
  */
-public class MyIntro {
+public class MyIntro extends AppIntro {
+
+    public void init(Bundle savedInstanceState) {
+        addSlide(AppIntroFragment.newInstance("Title here", "Description here...\nYeah, I've added this fragment programmatically",
+                R.drawable.ic_slide1, Color.parseColor("#2196F3")));
+
+        addSlide(AppIntroFragment.newInstance("HTML Description", Html.fromHtml("<b>Description bold...</b><br><i>Description italic...</i>"),
+                R.drawable.ic_slide1, Color.parseColor("#2196F3")));
+
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+        showSkipButton(false);
+
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+    private void loadMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        loadMainActivity();
+        Toast.makeText(getApplicationContext(), getString(R.string.skip), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNextPressed() {
+
+    }
+
+    @Override
+    public void onDonePressed() {
+        loadMainActivity();
+    }
+
+    @Override
+    public void onSlideChanged() {
+
+    }
+
+    public void getStarted(View v){
+        loadMainActivity();
+    }
+}
 }
