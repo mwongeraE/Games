b973e50b144cbbea97ec3ee5f7184060d04547a8
mwongeraE <evansmwongera@gmail.com>
2016-03-20 21:11:05 +0300

    mainactivity cardui

--- app/src/main/java/com/example/mwongera/games/MainActivity.java
+++ app/src/main/java/com/example/mwongera/games/MainActivity.java
@@ -9,13 +9,53 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private List<rowitem> rowItems;
+    private static Integer[] images = {
+            R.drawable.red,
+            R.drawable.red,
+            R.drawable.red,
+            R.drawable.red,
+            R.drawable.red,
+            R.drawable.red,
+            R.drawable.red,
+            R.drawable.red
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Intialize and set the Action Bar Background to Holo Blue
+        ActionBar actionBar = getActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#33b5e5" )));
+
+        ListView lv = (ListView) findViewById(R.id.myList);
+        rowItems = new ArrayList<rowitem>();
+
+        String[] titles = {"Movie1","Movie2","Movie3","Movie4","Movie5","Movie6","Movie7","Movie8"};
+        String[] descriptions = {"First Movie","Second movie","Third Movie","Fourth Movie","Fifth Movie",
+                "Sixth Movie","Seventh Movie","Eighth Movie"};
+
+        //Populate the List
+        for (int i = 0; i < titles.length; i++) {
+            RowItem item = new RowItem(images[i], titles[i], descriptions[i]);
+            rowItems.add(item);
+        }
+
+        // Set the adapter on the ListView
+        LazyAdapter adapter = new LazyAdapter(getApplicationContext(), R.layout.list_row, rowItems);
+        lv.setAdapter(adapter);
     }
 
     public void startDefaultIntro(View v) {
