21d6674d6494c8236e6a7e7325803291f400e49f
mwongeraE <evansmwongera@gmail.com>
2016-03-20 21:13:29 +0300

    rowitem

---
+++ app/src/main/java/com/example/mwongera/games/RowItem.java
@@ -0,0 +1,39 @@
+package com.example.mwongera.games;
+
+/**
+ * Created by mwongera on 3/20/16.
+ */
+
+public class RowItem {
+    private int imageId;
+    private String title;
+    private String description;
+
+    public RowItem(int imageId, String title, String desc) {
+        this.imageId = imageId;
+        this.title = title;
+        this.description = desc;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+    public String getDesc() {
+        return description;
+    }
+    public void setDesc(String desc) {
+        this.description = desc;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @Override
+    public String toString() {
+        return title + "\n" + description;
+    }
+}
